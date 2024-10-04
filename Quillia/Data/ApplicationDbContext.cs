81aab5ee86545760ed6f0fe4ef83ce182f089be3
Frzz23 <frenzdrez@gmail.com>
2024-10-04 21:17:50 +0545

    CRUD Operation

---
+++ Quillia/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quillia.Models;
+
+namespace Quillia.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categorycs> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Categorycs>().HasData(
+                new Categorycs { Id = 1, Name = "action",  DisplayOrder = 1 },
+                new Categorycs { Id = 2, Name = "scifi",   DisplayOrder = 2 },
+                new Categorycs { Id = 3, Name = "history", DisplayOrder = 3 }
+                );
+        }
+    }
+}
