224c50d74eae252ca673067b1c43118bf679117b
Frzz23 <frenzdrez@gmail.com>
2024-10-11 09:52:17 +0545

    Home&Details_Section

--- Quillia/Areas/Customer/Controllers/HomeController.cs
+++ Quillia/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Quillia.Database.Repositary.IRepository;
 using Quillia.Models;
 using System.Diagnostics;
 
@@ -8,17 +9,24 @@ namespace Quillia.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
